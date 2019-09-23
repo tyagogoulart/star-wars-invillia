7a5d06ba35db5d8e8a6f3a2c6d043b1b04766135
Tyago Goulart <tyagogoulartn@gmail.com>
2019-09-23 01:42:35 -0300

    adding utils functions to project

---
+++ src/util/peopleImages.js
@@ -0,0 +1,51 @@
+// Infelizmente, por falta de tempo, não consegui deixar integrado 
+// com a Custom Search API do Google para carregar as imagens dos personagens.
+// Por isso criei este mock apenas para exibir os 10 primeiros da lista com imagem
+// para exemplificar como eu imaginei que os cards ficariam com as imagens.
+
+export default [
+    {
+        name: "Luke Skywalker",
+        path: "https://www.sideshow.com/storage/product-images/903109/luke-skywalker_star-wars_feature.jpg"
+    },
+    {
+        name: "C-3PO",
+        path: "https://www.sideshow.com/storage/product-images/2171/c-3po_star-wars_feature.jpg"
+    },
+    {
+        name: "R2-D2",
+        path: "https://www.rollingstone.com/wp-content/uploads/2018/06/gettyimages-657986950-1d60f553-a3e9-4d80-aa88-4fd867a98036.jpg"
+    },
+    {
+        name: "Darth Vader",
+        path: "https://images.fatherly.com/wp-content/uploads/2018/12/darthvader-header.jpg"
+    },
+    {
+        name: "Leia Organa",
+        path: "https://lumiere-a.akamaihd.net/v1/images/leia-organa-feature-image_d0f5e953.jpeg?region=0%2C0%2C1280%2C720&width=960"
+    },
+    {
+        name: "Owen Lars",
+        path: "https://img.ksl.com/slc/2511/251162/25116237.jpg"
+    },
+    {
+        name: "Beru Whitesun lars",
+        path: "https://imagesvc.timeincapp.com/v3/fan/image?url=https%3A%2F%2Fculturess.com%2Ffiles%2F2017%2F06%2Faunt-beru.jpg&c=sc&w=736&h=485"
+    },
+    {
+        name: "R5-D4",
+        path: "https://www.jeditemplearchives.com/galleries/2011/Review_R5D4TVC/Review_R5D4TVC_stillA.JPG"
+    },
+    {
+        name: "Biggs Darklighter",
+        path: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiv-JJo3KoD6DzQ6E-JKCuOB519RhdoZFS3Q4RbNvig-1ZkzVyxw"
+    },
+    {
+        name: "Obi-Wan Kenobi",
+        path: "http://www.tenhomaisdiscosqueamigos.com/wp-content/uploads/2019/08/ewan-mcgregor-obi-wan-kenobi.jpg"
+    },
+
+
+    
+    
+]
